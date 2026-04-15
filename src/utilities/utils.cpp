1af8a227d1b4426da0206064dec7c2a17908e848
Michał Rodak <xoriner@gmail.com>
2026-04-15 23:33:17 +0200

    utils fix

--- src/utilities/utils.cpp
+++ src/utilities/utils.cpp
@@ -8,7 +8,7 @@
 
 #include "tsp_result.h"
 
-// Funkcja pomocnicza do czyszczenia stringów z białych znaków
+// Funkcja pomocnicza do czyszczenia stringów z białych znaków (trim)
 std::string trim(const std::string& str) {
     size_t first = str.find_first_not_of(" \t\r\n");
     if (std::string::npos == first) return str;
@@ -16,6 +16,7 @@ std::string trim(const std::string& str) {
     return str.substr(first, (last - first + 1));
 }
 
+// Funkcja dla prostych plików tekstowych (np. z zajęć)
 std::vector<std::vector<int>> read_simple_input(std::string filename) {
     std::ifstream input_file(filename);
     if (!input_file.is_open()) {
@@ -41,6 +42,7 @@ std::vector<std::vector<int>> read_simple_input(std::string filename) {
     return graph_matrix;
 }
 
+// Przeliczanie współrzędnych geograficznych (wymagane dla plików ulysses16/22)
 int calc_geo_dist(double x1, double y1, double x2, double y2) {
     double PI = 3.14159265358979323846;
     auto to_rad = [PI](double deg) {
@@ -59,6 +61,7 @@ int calc_geo_dist(double x1, double y1, double x2, double y2) {
     return (int)(RRR * acos(0.5 * ((1.0 + q1) * q2 - (1.0 - q1) * q3)) + 1.0);
 }
 
+// Główna funkcja obsługująca formaty TSPLIB (.atsp i .tsp)
 std::vector<std::vector<int>> read_tsplib(std::string filename) {
     std::ifstream file(filename);
     if (!file.is_open()) {
@@ -66,60 +69,74 @@ std::vector<std::vector<int>> read_tsplib(std::string filename) {
         exit(1);
     }
 
-    std::string line, weight_type = "", weight_format = "";
+    std::string line;
     int dimension = 0;
-    bool is_explicit = false;
+    std::string weight_type = "";
+    std::string weight_format = "";
+    bool explicit_data = false;
 
-    // 1. Parsowanie Nagłówka
+    // --- 1. Parsowanie Nagłówka ---
     while (std::getline(file, line)) {
-        line = trim(line);
-        std::string line_upper = line;
-        std::transform(line_upper.begin(), line_upper.end(), line_upper.begin(), ::toupper);
+        std::string line_up = trim(line);
+        std::transform(line_up.begin(), line_up.end(), line_up.begin(), ::toupper);
 
-        if (line_upper.find("DIMENSION") != std::string::npos) {
-            size_t pos = line_upper.find(":");
-            dimension = std::stoi(line_upper.substr(pos != std::string::npos ? pos + 1 : 9));
+        if (line_up.empty()) continue;
+
+        if (line_up.find("DIMENSION") != std::string::npos) {
+            size_t pos = line_up.find(":");
+            if (pos != std::string::npos) {
+                dimension = std::stoi(trim(line_up.substr(pos + 1)));
+            } else {
+                // Obsługa formatu bez dwukropka
+                std::stringstream ss(line_up);
+                std::string tmp;
+                ss >> tmp >> dimension;
+            }
         }
-        else if (line_upper.find("EDGE_WEIGHT_TYPE") != std::string::npos) {
-            if (line_upper.find("GEO") != std::string::npos) weight_type = "GEO";
-            if (line_upper.find("EUC_2D") != std::string::npos) weight_type = "EUC_2D";
-            if (line_upper.find("EXPLICIT") != std::string::npos) is_explicit = true;
+        else if (line_up.find("EDGE_WEIGHT_TYPE") != std::string::npos) {
+            if (line_up.find("GEO") != std::string::npos) weight_type = "GEO";
+            else if (line_up.find("EUC_2D") != std::string::npos) weight_type = "EUC_2D";
+            else if (line_up.find("EXPLICIT") != std::string::npos) explicit_data = true;
         }
-        else if (line_upper.find("EDGE_WEIGHT_FORMAT") != std::string::npos) {
-            if (line_upper.find("LOWER_DIAG_ROW") != std::string::npos) weight_format = "LOWER_DIAG_ROW";
-            if (line_upper.find("FULL_MATRIX") != std::string::npos) weight_format = "FULL_MATRIX";
+        else if (line_up.find("EDGE_WEIGHT_FORMAT") != std::string::npos) {
+            if (line_up.find("LOWER_DIAG_ROW") != std::string::npos) weight_format = "LOWER_DIAG_ROW";
+            else if (line_up.find("FULL_MATRIX") != std::string::npos) weight_format = "FULL_MATRIX";
         }
-        else if (line_upper.find("EDGE_WEIGHT_SECTION") != std::string::npos ||
-                 line_upper.find("NODE_COORD_SECTION") != std::string::npos) {
-            break;
+        else if (line_up.find("EDGE_WEIGHT_SECTION") != std::string::npos ||
+                 line_up.find("NODE_COORD_SECTION") != std::string::npos) {
+            break; // Rozpoczynamy wczytywanie danych numerycznych
         }
     }
 
-    if (dimension == 0) exit(1);
+    if (dimension <= 0) {
+        std::cerr << "Error: Could not determine dimension of " << filename << std::endl;
+        exit(1);
+    }
+
     std::vector<std::vector<int>> matrix(dimension, std::vector<int>(dimension, 0));
 
-    // 2. Wczytywanie danych
-    if (is_explicit) {
+    // --- 2. Wczytywanie Danych ---
+    if (explicit_data) {
         if (weight_format == "LOWER_DIAG_ROW") {
-            // Obsługa formatu trójkątnego (gr17, gr21)
+            // Obsługa gr17.tsp, gr21.tsp (macierze symetryczne trójkątne)
             for (int i = 0; i < dimension; i++) {
                 for (int j = 0; j <= i; j++) {
                     int val;
                     if (!(file >> val)) break;
                     matrix[i][j] = val;
-                    matrix[j][i] = val; // Symetria
+                    matrix[j][i] = val;
                 }
             }
         } else {
-            // Domyślnie FULL_MATRIX (br17)
+            // Obsługa br17.atsp, ftv33.atsp, ftv35.atsp itd. (macierze pełne)
             for (int i = 0; i < dimension; i++) {
                 for (int j = 0; j < dimension; j++) {
-                    file >> matrix[i][j];
+                    if (!(file >> matrix[i][j])) break;
                 }
             }
         }
     } else {
-        // Czytamy współrzędne (ulysses, fri26, berlin52)
+        // Obsługa ulysses16.tsp, ulysses22.tsp, fri26.tsp (współrzędne)
         std::vector<std::pair<double, double>> coords(dimension);
         for (int i = 0; i < dimension; i++) {
             int id;
@@ -128,8 +145,9 @@ std::vector<std::vector<int>> read_tsplib(std::string filename) {
 
         for (int i = 0; i < dimension; i++) {
             for (int j = 0; j < dimension; j++) {
-                if (i == j) matrix[i][j] = 0;
-                else if (weight_type == "GEO") {
+                if (i == j) {
+                    matrix[i][j] = 0;
+                } else if (weight_type == "GEO") {
                     matrix[i][j] = calc_geo_dist(coords[i].first, coords[i].second, coords[j].first, coords[j].second);
                 } else {
                     double dx = coords[i].first - coords[j].first;
@@ -139,6 +157,7 @@ std::vector<std::vector<int>> read_tsplib(std::string filename) {
             }
         }
     }
+
     return matrix;
 }
 
